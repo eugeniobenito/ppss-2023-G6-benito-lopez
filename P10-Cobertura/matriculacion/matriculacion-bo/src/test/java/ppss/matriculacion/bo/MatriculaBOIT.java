d987f78cc357b70a609241eb0a7c8c42e07856de
Eugenio Benito Lopez <ebl29@alu.ua.es>
2023-05-26 20:52:03 +0200

    P10 terminada

---
+++ P10-Cobertura/matriculacion/matriculacion-bo/src/test/java/ppss/matriculacion/bo/MatriculaBOIT.java
@@ -0,0 +1,14 @@
+package ppss.matriculacion.bo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatriculaBOIT {
+
+    @Test
+    public void integrationTest1() {
+        assertTrue(true);
+    }
+
+}
---
+++ P10-Cobertura/matriculacion/matriculacion-proxy/src/test/java/ppss/matriculacion/proxy/ProxyTest.java
@@ -0,0 +1,14 @@
+package ppss.matriculacion.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProxyTest {
+
+    @Test
+    public void integrationTest1() {
+        assertTrue(true);
+    }
+
+}
